6dda01632e828f0b74b926a56664bea84aba299c
Dawit Getachew <dgetachew@paga.com>
2021-07-06 22:38:53 +0300

    initial commit

---
+++ src/components/repos/Repos.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import RepoItem from './RepoItem';
+import GithubContext from '../../context/github/githubContext';
+
+
+const Repos = () => {
+    const githubContext = useContext(GithubContext);
+    return githubContext.repos.map(repo => <RepoItem repo={repo} key={repo.id} />);
+};
+
+Repos.propTypes = {
+    repos: PropTypes.array.isRequired
+};
+
+export default Repos;
